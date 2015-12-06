f184bc6b3945ad063458fbaa8b5c6d13c0dbe25b
elifoster <elifosterwy@gmail.com>
2015-12-05 21:51:25 -0800

    :gem: A bunch of new list query methods: * get_all_pages_in_namespace * get_all_users * get_all_blocks * get_all_transcluders * get_all_deleted_files * get_all_protected_titles * get_user_contributions * get_full_watchlist Close #8 finally.

--- lib/mediawiki/query/lists.rb
+++ lib/mediawiki/query/lists.rb
@@ -304,6 +304,176 @@ module MediaWiki
 
         ret
       end
+
+      # Gets all pages within a namespace integer.
+      # @param namespace [Int] The namespace ID.
+      # @param limit [Int] See #get_all_images
+      # @return [Array] An array of all page titles.
+      def get_all_pages_in_namespace(namespace, limit = 500)
+        params = {
+          action: 'query',
+          list: 'allpages',
+          apnamespace: namespace,
+          aplimit: MediaWiki::Query.get_limited(limit)
+        }
+
+        response = post(params)
+
+        ret = []
+        response['query']['allpages'].each { |p| ret.push(p['title']) }
+
+        ret
+      end
+
+      # Gets all users, or all users in a group.
+      # @param group [String] The group to limit this query to.
+      # @param limit [Int] See #get_all_images.
+      # @return [Hash] A hash of all users, names are keys, IDs are values.
+      def get_all_users(group = nil, limit = 500)
+        params = {
+          action: 'query',
+          list: 'allusers',
+          aulimit: MediaWiki::Query.get_limited(limit)
+        }
+        params[:augroup] = group unless group.nil?
+
+        response = post(params)
+
+        ret = {}
+        response['query']['allusers'].each { |u| ret[u['name']] = u['userid'] }
+
+        ret
+      end
+
+      # Gets all block IDs on the wiki. It seems like this only gets non-IP
+      #   blocks, but the MediaWiki docs are a bit unclear.
+      # @param limit [Int] See #get_all_images.
+      # @return [Array] All block IDs as strings.
+      def get_all_blocks(limit = 500)
+        params = {
+          action: 'query',
+          list: 'blocks',
+          bklimit: MediaWiki::Query.get_limited(limit),
+          bkprop: 'id'
+        }
+
+        response = post(params)
+
+        ret = []
+        response['query']['blocks'].each { |b| ret.push(b['id']) }
+
+        ret
+      end
+
+      # Gets all page titles that transclude a given page.
+      # @param page [String] The page name.
+      # @param limit [Int] See #get_all_images.
+      # @return [Array] All transcluder page titles.
+      def get_all_transcluders(page, limit = 500)
+        params = {
+          action: 'query',
+          list: 'embeddedin',
+          eititle: page,
+          eilimit: MediaWiki::Query.get_limited(limit)
+        }
+
+        response = post(params)
+
+        ret = []
+        response['query']['embeddedin'].each { |e| ret.push(e['title']) }
+
+        ret
+      end
+
+      # Gets an array of all deleted or archived files on the wiki.
+      # @param limit [Int] See #get_all_images
+      # @return [Array] All deleted file names. These are not titles, so they do
+      #   not include "File:".
+      def get_all_deleted_files(limit = 500)
+        params = {
+          action: 'query',
+          list: 'filearchive',
+          falimit: MediaWiki::Query.get_limited(limit)
+        }
+
+        response = post(params)
+
+        ret = []
+        response['query']['filearchive'].each { |f| ret.push(f['name']) }
+
+        ret
+      end
+
+      # Gets a list of all protected pages, by protection level if provided.
+      # @param protection_level [String] The protection level, e.g., sysop
+      # @param limit [Int] See #get_all_images.
+      # @return [Array] All protected page titles.
+      def get_all_protected_titles(protection_level = nil, limit = 500)
+        params = {
+          action: 'query',
+          list: 'protectedtitles',
+          ptlimit: MediaWiki::Query.get_limited(limit)
+        }
+        params[:ptlevel] = protection_level unless protection_level.nil?
+
+        response = post(params)
+
+        ret = []
+        response['query']['protectedtitles'].each { |t| ret.push(t['title']) }
+
+        ret
+      end
+
+      # Gets the latest contributions by the user until the limit.
+      # @param user [String] The username.
+      # @param limit [Int] See #get_all_images.
+      # @return [Hash] Each contribution by its revid, containing the title,
+      #   summary, total contribution size, and the size change relative to the
+      #   previous edit.
+      def get_user_contributions(user, limit = 500)
+        params = {
+          action: 'query',
+          list: 'usercontribs',
+          ucuser: user,
+          uclimit: MediaWiki::Query.get_limited(limit),
+          ucprop: 'ids|title|comment|size|sizediff|flags|patrolled'
+        }
+
+        response = post(params)
+
+        ret = {}
+        response['query']['usercontribs'].each do |item|
+          ret[item['revid']] = {
+            title: item['title'],
+            summary: item['comment'],
+            total_size: item['size'],
+            diff_size: item['sizediff']
+          }
+        end
+
+        ret
+      end
+
+      # Gets the user's full watchlist. If no user is provided, it will use the
+      #   currently logged in user, according to the MediaWiki API.
+      # @param user [String] The username.
+      # @param limit [Int] See #get_all_images.
+      # @return [Array] All the watchlist page titles.
+      def get_full_watchlist(user = nil, limit = 500)
+        params = {
+          action: 'query',
+          list: 'watchlist',
+          wlprop: 'title'
+        }
+        params[:wluser] = user unless user.nil?
+
+        response = post(params)
+
+        ret = []
+        response['query']['watchlist'].each { |t| ret.push(t['title']) }
+
+        ret
+      end
     end
   end
 end
