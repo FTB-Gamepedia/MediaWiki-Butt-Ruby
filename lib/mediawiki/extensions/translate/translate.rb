42b122245be261347c56ca2b94658fe85877a019
elifoster <elifosterwy@gmail.com>
2015-12-07 13:59:01 -0800

    :bug: Fix some issues with the params before we forget

--- lib/mediawiki/extensions/translate/translate.rb
+++ lib/mediawiki/extensions/translate/translate.rb
@@ -4,17 +4,15 @@ module MediaWiki
       # Gets the results from a language search.
       # It searches for every language in every language.
       # @param search [String] The search string.
-      # @param typos [Int] Number of typos allowed. Optional. Web API defaults it to 1.
+      # @param typos [Int] Number of typos allowed. Optional.
       # @return [Hash] The result, in format code -> name.
-      def language_search(search, *typos)
+      def language_search(search, typos = 1)
         params = {
           action: 'languagesearch',
           search: search,
-          format: 'json'
+          typos: typos
         }
-        
-        params[:typos] = typos if defined? typos
-          
+
         response = post(params)['languagesearch']
       end
     end
