54fa2f421d6c6c50e44feca850d50c90735000ec
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2016-06-16 14:28:37 -0700

    :wrench: :fu: Authentication improvements (#39)
    Authentication/account creation functions are now called recursively, instead of having a bunch of duplicate code for handling tokens. The error throwing functions are now private because that does not need to be used by people.

--- lib/mediawiki/auth.rb
+++ lib/mediawiki/auth.rb
@@ -2,10 +2,138 @@ require_relative 'exceptions'
 
 module MediaWiki
   module Auth
+    # Logs the user into the wiki. This is generally required for editing and getting restricted data.
+    # @param username [String] The username
+    # @param password [String] The password
+    # @param token [String] The login token to use. Only set this if you know what you are doing. You probably want
+    # to just let the function get the token and set it on its own.
+    # @see #check_login
+    # @see https://www.mediawiki.org/wiki/API:Login MediaWiki Login API Docs
+    # @since 0.1.0
+    # @return [Boolean] True if the login was successful, false if not.
+    def login(username, password, token = nil)
+      params = {
+        action: 'login',
+        lgname: username,
+        lgpassword: password
+      }
+      params[:lgtoken] = token if token
+      header = {}
+      header['Set-Cookie'] = @cookie if @cookie
+
+      response = post(params, true, header)
+      result = response['login']['result']
+
+      if result == 'NeedToken'
+        token = response['login']['token']
+        return login(username, password, token)
+      end
+
+      if check_login(result)
+        @cookie = "#{response['login']['cookieprefix']}Session=#{response['login']['sessionid']}"
+        @name = username
+        @logged_in = true
+        return true
+      end
+
+      false
+    end
+
+    # Logs the current user out.
+    # @see https://www.mediawiki.org/wiki/API:Logout MediaWiki Logout API Docs
+    # @since 0.1.0
+    # @return [Boolean] True if it was able to log anyone out, false if not.
+    def logout
+      if @logged_in
+        params = {
+          action: 'logout'
+        }
+
+        post(params)
+        @logged_in = false
+
+        true
+      else
+        false
+      end
+    end
+
+    # Creates an account using the standard procedure.
+    # @param username [String] The desired username.
+    # @param password [String] The desired password.
+    # @param language [String] The language code to be set as default for the account. Defaults to 'en', or English.
+    # Use the language code, not the name.
+    # @param reason [String] The reason for creating the account, as shown in the account creation log. Optional.
+    # @param token [String] The account creation token to use. Only set this if you know what you are doing. You
+    # probably want to just let the function get the token and set it on its own.
+    # @see #check_create
+    # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
+    # @since 0.1.0
+    # @return [Boolean] True if successful, false if not.
+    def create_account(username, password, language = 'en', reason = nil, token = '')
+      params = {
+        name: username,
+        password: password,
+        language: language,
+        token: token
+      }
+      params[:reason] = reason unless reason.nil?
+
+      result = post(params)
+      unless result['error'].nil?
+        check_create(result['error']['code'])
+        return false
+      end
+
+      if result['createaccount']['result'] == 'Success'
+        @tokens.clear
+
+        return true
+      elsif result['createaccount']['result'] == 'NeedToken'
+        return create_account(username, password, language, reason, result['createaccount']['token'])
+      end
+
+      false
+    end
+
+    # Creates an account using the random password sent by email procedure.
+    # @param email [String] The desired email address
+    # @param (see #create_account)
+    # @see #check_create
+    # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
+    # @since 0.1.0
+    # @return [Boolean] True if successful, false if not.
+    def create_account_email(username, email, language = 'en', reason = nil, token = '')
+      params = {
+        name: username,
+        email: email,
+        mailpassword: 'value',
+        language: language,
+        token: token
+      }
+      params[:reason] = reason unless reason.nil?
+
+      result = post(params)
+      unless result['error'].nil?
+        check_create(result['error']['code'])
+        return false
+      end
+
+      if result['createaccount']['result'] == 'Success'
+        @tokens.clear
+
+        return true
+      elsif result['createaccount']['result'] == 'NeedToken'
+        return create_account_email(username, email, language, reason, result['createaccount']['token'])
+      end
+
+      false
+    end
+
+    private
+
     # Checks the login result for errors and raises errors accordingly.
     # @param result [String] The parsed version of the result.
-    # @param secondtry [Boolean] Whether this login is the first or second try. False for first, true for second.
-    # @raise [NeedTokenMoreThanOnceError] When secondtry is true and the wiki still returns 'NeedToken'
     # @raise [NoNameError] When the username is nil or undefined.
     # @raise [IllegalUsernameError] When the username is illegal.
     # @raise [UsernameNotExistsError] When the username does not exist.
@@ -18,13 +146,11 @@ module MediaWiki
     # @raise [BlockedError] When the user is blocked from the wiki.
     # @since 0.1.0
     # @return [Boolean] true if successful. Does not return anything otherwise.
-    def check_login(result, secondtry)
+    def check_login(result)
       case result
       when 'Success'
         @logged_in = true
         return true
-      when 'NeedToken'
-        fail MediaWiki::Butt::NeedTokenMoreThanOnceError if secondtry
       when 'NoName'
         fail MediaWiki::Butt::NoNameError
       when 'Illegal'
@@ -44,6 +170,8 @@ module MediaWiki
       when 'Blocked'
         fail MediaWiki::Butt::BlockedError
       end
+
+      false
     end
 
     # Checks the account creation result's error and raises the corresponding exception.
@@ -91,163 +219,6 @@ module MediaWiki
         fail MediaWiki::Butt::PermDeniedError
       when 'createaccount-hook-aborted'
         fail MediaWiki::Butt::HookAbortedError
-      end
-    end
-
-    # Logs the user into the wiki. This is generally required for editing and getting restricted data.
-    # @param username [String] The username
-    # @param password [String] The password
-    # @see #check_login
-    # @see https://www.mediawiki.org/wiki/API:Login MediaWiki Login API Docs
-    # @since 0.1.0
-    # @return [Boolean] True if the login was successful, false if not.
-    def login(username, password)
-      params = {
-        action: 'login',
-        lgname: username,
-        lgpassword: password
-      }
-
-      result = post(params)
-      if check_login(result['login']['result'], false)
-        @logged_in = true
-        true
-      elsif result['login']['result'] == 'NeedToken' &&
-            !result['login']['token'].nil?
-        token = result['login']['token']
-        token_params = {
-          action: 'login',
-          lgname: username,
-          lgpassword: password,
-          lgtoken: token
-        }
-
-        # Consider refactor the @cookie initialization.
-        @cookie = "#{result['login']['cookieprefix']}Session=#{result['login']['sessionid']}"
-        result = post(token_params, true, 'Set-Cookie' => @cookie)
-        check = check_login(result['login']['result'], true)
-        @name = username if check
-        return check
-      end
-
-      false
-    end
-
-    # Logs the current user out.
-    # @see https://www.mediawiki.org/wiki/API:Logout MediaWiki Logout API Docs
-    # @since 0.1.0
-    # @return [Boolean] True if it was able to log anyone out, false if not.
-    def logout
-      if @logged_in
-        params = {
-          action: 'logout'
-        }
-
-        post(params)
-        @logged_in = false
-
-        true
-      else
-        false
-      end
-    end
-
-    # Creates an account using the standard procedure.
-    # @param username [String] The desired username.
-    # @param password [String] The desired password.
-    # @param language [String] The language code to be set as default for the account. Defaults to 'en', or English.
-    # Use the language code, not the name.
-    # @param reason [String] The reason for creating the account, as shown in the account creation log. Optional.
-    # @see #check_create
-    # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
-    # @since 0.1.0
-    # @return [Boolean] True if successful, false if not.
-    def create_account(username, password, language = 'en', reason = nil)
-      params = {
-        name: username,
-        password: password,
-        language: language,
-        token: ''
-      }
-      params[:reason] = reason unless reason.nil?
-
-      result = post(params)
-      unless result['error'].nil?
-        check_create(result['error']['code'])
-        return false
-      end
-
-      if result['createaccount']['result'] == 'Success'
-        @tokens.clear
-
-        true
-      elsif result['createaccount']['result'] == 'NeedToken'
-        params = {
-          name: username,
-          password: password,
-          reason: reason,
-          language: language,
-          token: result['createaccount']['token']
-        }
-
-        result = post(params, true, true)
-        if !result['error'].nil?
-          check_create(result['error']['code'])
-          return false
-        elsif result['createaccount']['result'] == 'Success'
-          return true
-        else
-          return false
-        end
-      end
-    end
-
-    # Creates an account using the random password sent by email procedure.
-    # @param email [String] The desired email address
-    # @param (see #create_account)
-    # @see #check_create
-    # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
-    # @since 0.1.0
-    # @return [Boolean] True if successful, false if not.
-    def create_account_email(username, email, language = 'en', reason = nil)
-      params = {
-        name: username,
-        email: email,
-        mailpassword: 'value',
-        language: language,
-        token: ''
-      }
-      params[:reason] = reason unless reason.nil?
-
-      result = post(params)
-      unless result['error'].nil?
-        check_create(result['error']['code'])
-        return false
-      end
-
-      if result['createaccount']['result'] == 'Success'
-        @tokens.clear
-
-        true
-      elsif result['createaccount']['result'] == 'NeedToken'
-        params = {
-          name: username,
-          email: email,
-          mailpassword: 'value',
-          reason: reason,
-          language: language,
-          token: result['createaccount']['token']
-        }
-
-        result = post(params, true, true)
-        if !result['error'].nil?
-          check_create(result['error']['code'])
-          return false
-        elsif result['createaccount']['result'] == 'Success'
-          return true
-        else
-          return false
-        end
       end
     end
   end
