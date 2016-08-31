65b7e7d2be19b2ef1681aba41547b643810ae943
elifoster <elifosterwy@gmail.com>
2016-08-30 22:52:31 -0700

    :bug: Fix query methods throwing NoMethodErrors. Unless statements are weird. I haven't been able to find sources to provide that document this, but from some testing it appears that || and && function *differently* in unless statements than if statements. 'unless false || true' and 'unless true || false' will not execute, but 'unless true && false' or 'unless false && true' will. I decided to simply break the break statement into two, because it is much more readable than 'break unless use_continuation && result.key?('continue')'. Many style guides support this, probably for both reasons I provided. cc @xbony2

--- lib/mediawiki/butt.rb
+++ lib/mediawiki/butt.rb
@@ -79,7 +79,8 @@ module MediaWiki
       loop do
         result = post(params)
         yield(base_return, result['query']) if result.key?('query')
-        break unless @use_continuation || result.key?('continue')
+        break unless @use_continuation
+        break unless result.key?('continue')
         continue = result['continue']
         continue.each do |key, val|
           params[key.to_sym] = val
