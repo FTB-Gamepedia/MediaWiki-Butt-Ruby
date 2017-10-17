6850f04f2644a19624e1275eecd7f1433c8d609b
elifoster <elifosterwy@gmail.com>
2017-10-16 17:09:05 -0700

    Update token system to reflect latest MediaWiki This resolves #45, resolves #46, and resolves part of #65.

--- lib/mediawiki/auth.rb
+++ lib/mediawiki/auth.rb
@@ -5,34 +5,23 @@ module MediaWiki
     # Logs the user into the wiki. This is generally required for editing and getting restricted data.
     # @param username [String] The username
     # @param password [String] The password
-    # @param token [String] The login token to use. Only set this if you know what you are doing. You probably want
-    # to just let the function get the token and set it on its own.
-    # @see #check_login
     # @see https://www.mediawiki.org/wiki/API:Login MediaWiki Login API Docs
     # @since 0.1.0
     # @raise [AuthenticationError]
     # @return [Boolean] True if the login was successful.
-    def login(username, password, token = nil)
+    def login(username, password)
       params = {
         action: 'login',
         lgname: username,
-        lgpassword: password
+        lgpassword: password,
+        lgtoken: get_token('login')
       }
-      params[:lgtoken] = token if token
-      header = {}
-      header['Set-Cookie'] = @cookie if @cookie
 
-      response = post(params, true, header, true)
+      response = post(params)
       result = response['login']['result']
 
-      if result == 'NeedToken'
-        token = response['login']['token']
-        return login(username, password, token)
-      end
-
       if result == 'Success'
-        @cookie = "#{response['login']['cookieprefix']}Session=#{response['login']['sessionid']}"
-        @name = username
+        @name = response['login']['lgusername']
         @logged_in = true
         return true
       end
@@ -65,18 +54,16 @@ module MediaWiki
     # @param language [String] The language code to be set as default for the account. Defaults to 'en', or English.
     # Use the language code, not the name.
     # @param reason [String] The reason for creating the account, as shown in the account creation log. Optional.
-    # @param token [String] The account creation token to use. Only set this if you know what you are doing. You
-    # probably want to just let the function get the token and set it on its own.
     # @see #check_create
     # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
     # @since 0.1.0
     # @return [Boolean] True if successful, false if not.
-    def create_account(username, password, language = 'en', reason = nil, token = '')
+    def create_account(username, password, language = 'en', reason = nil)
       params = {
         name: username,
         password: password,
         language: language,
-        token: token
+        token: get_token('createaccount')
       }
       params[:reason] = reason unless reason.nil?
 
@@ -85,15 +72,7 @@ module MediaWiki
         raise MediaWiki::Butt::AuthenticationError.new(result['error']['code'])
       end
 
-      if result['createaccount']['result'] == 'Success'
-        @tokens.clear
-
-        return true
-      elsif result['createaccount']['result'] == 'NeedToken'
-        return create_account(username, password, language, reason, result['createaccount']['token'])
-      end
-
-      false
+      result['createaccount']['result'] == 'Success'
     end
 
     # Creates an account using the random password sent by email procedure.
@@ -103,13 +82,13 @@ module MediaWiki
     # @see https://www.mediawiki.org/wiki/API:Account_creation MediaWiki Account Creation Docs
     # @since 0.1.0
     # @return [Boolean] True if successful, false if not.
-    def create_account_email(username, email, language = 'en', reason = nil, token = '')
+    def create_account_email(username, email, language = 'en', reason = nil)
       params = {
         name: username,
         email: email,
         mailpassword: 'value',
         language: language,
-        token: token
+        token: get_token('createaccount')
       }
       params[:reason] = reason unless reason.nil?
 
@@ -118,15 +97,7 @@ module MediaWiki
         raise MediaWiki::Butt::AuthenticationError.new(result['error']['code'])
       end
 
-      if result['createaccount']['result'] == 'Success'
-        @tokens.clear
-
-        return true
-      elsif result['createaccount']['result'] == 'NeedToken'
-        return create_account_email(username, email, language, reason, result['createaccount']['token'])
-      end
-
-      false
+      result['createaccount']['result'] == 'Success'
     end
   end
 end
--- lib/mediawiki/query/meta/meta.rb
+++ lib/mediawiki/query/meta/meta.rb
@@ -8,6 +8,33 @@ module MediaWiki
       include MediaWiki::Query::Meta::UserInfo
       include MediaWiki::Query::Meta::FileRepoInfo
       include MediaWiki::Query::Meta::UserInfo
+
+      # All valid types of tokens. Taken from API:Tokens on MediaWiki.
+      TOKEN_TYPES = %w(csrf watch patrol rollback userrights login createaccount)
+
+      # Obtains a token for the current user (or lack thereof) for specific actions. This uses the functionality
+      # introduced in MediaWiki 1.27
+      # @param type [String, Array<String>] The type of token to get. See #TOKEN_TYPES to see the valid types. If it
+      #   is invalid, it will default to 'csrf'. If it is an array, it will join by a pipe for the API.
+      # @return [String, Hash<String => String>] Either a token or a set of tokens organized by type. If multiple
+      #   valid types are provided in the parameter, it returns a hash identical to the API response (see API docs).
+      def get_token(type = 'csrf')
+        params = {
+          action: 'query',
+          meta: 'tokens'
+        }
+
+        if type.is_a?(Array)
+          type = (type - TOKEN_TYPES).empty? ? type.join('|') : 'csrf'
+        else
+          type = TOKEN_TYPES.include?(type) ? type : 'csrf'
+        end
+        params[:type] = type
+
+        resp = post(params)
+        tokens = resp['query']['tokens']
+        tokens.size > 1 ? tokens : tokens["#{type}token"]
+      end
     end
   end
 end
